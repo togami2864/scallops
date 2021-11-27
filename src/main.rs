0447f78792841552039b6eef454f546e93974eb7
togami2864 <tuabobo123@gmail.com>
2021-11-27 15:45:34 +0900

    minimam sample

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use nix::sys::wait::*;
+use nix::unistd::{execv, fork, ForkResult};
+use std::ffi::CString;
 fn main() {
-    println!("Hello, world!");
+    match unsafe{fork()}  {
+        Ok(ForkResult::Parent{child, ..})=> {
+            match waitpid(child, None) {
+                Ok(WaitStatus::Exited(pid, status))=> {
+                    println!("exited: pid={:?}, status={:?}", pid, status);
+                }
+                Ok(WaitStatus::Signaled(pid, status, _)) => {
+                    println!("signaled: pid={:?}, status={:?}", pid, status)
+                }
+                _ => println!("exit"),
+            }
+
+        }
+        Ok(ForkResult::Child)=> {
+            let dir = CString::new("/bin/ls".to_string()).unwrap();
+            let arg = CString::new("-l".to_string()).unwrap();
+            execv(&dir, &[&dir, &arg]).unwrap();
+        }
+        Err(_) => panic!("Error")
+    }
 }
