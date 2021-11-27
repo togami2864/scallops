ca661e0524e5a7baf0db02fd344883bda30bcca5
togami2864 <tuabobo123@gmail.com>
2021-11-27 16:15:16 +0900

    feat: enable stdin

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,29 @@
 use nix::sys::wait::*;
 use nix::unistd::{execv, fork, ForkResult};
 use std::ffi::CString;
-fn main() {
-    match unsafe{fork()}  {
-        Ok(ForkResult::Parent{child, ..})=> {
-            match waitpid(child, None) {
-                Ok(WaitStatus::Exited(pid, status))=> {
-                    println!("exited: pid={:?}, status={:?}", pid, status);
-                }
-                Ok(WaitStatus::Signaled(pid, status, _)) => {
-                    println!("signaled: pid={:?}, status={:?}", pid, status)
-                }
-                _ => println!("exit"),
-            }
+use std::io::{stdin, stdout, Write};
 
-        }
-        Ok(ForkResult::Child)=> {
-            let dir = CString::new("/bin/ls".to_string()).unwrap();
-            let arg = CString::new("-l".to_string()).unwrap();
+fn main() {
+    print!("> ");
+    stdout().flush().unwrap();
+    let mut input = String::new();
+    stdin().read_line(&mut input).unwrap();
+    let mut parts = input.trim().split_whitespace();
+    let command = parts.next().unwrap();
+    let args = parts.next().unwrap();
+
+    match unsafe { fork() } {
+        Ok(ForkResult::Parent { child, .. }) => match waitpid(child, None) {
+            Ok(WaitStatus::Exited(pid, status)) => {
+                println!("exited: pid={:?}, status={:?}", pid, status);
+            }
+            _ => println!("exit"),
+        },
+        Ok(ForkResult::Child) => {
+            let dir = CString::new(command.to_string()).unwrap();
+            let arg = CString::new(args.to_string()).unwrap();
             execv(&dir, &[&dir, &arg]).unwrap();
         }
-        Err(_) => panic!("Error")
+        Err(_) => panic!("Error"),
     }
 }
